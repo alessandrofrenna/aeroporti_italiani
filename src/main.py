d37087036874563a0dd974a6b40fff2571f4e07b
Alessandro Frenna <alessandrofrenna95@gmail.com>
2021-06-01 01:46:35 +0200

    Prevent pipeline execution if files exist

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+from pipeline import files_are_present
 from pipeline.airports_traffic_data_extractor import extract_traffic_data
 from pipeline.italian_airports_data_extractor import extract_airports
 from pipeline.combinator import combine_airports_and_traffic_data
 
 if __name__ == '__main__':
-    extract_traffic_data()
-    extract_airports()
-    combine_airports_and_traffic_data()
+    if files_are_present():
+        print("Files already in the data directory, skipping elaboration pipeline")
+    else:
+        print("Files are not in the data directory, elaboration pipeline is starting...\n")
+        extract_traffic_data()
+        extract_airports()
+        combine_airports_and_traffic_data()
--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 INPUT_ENAV_2020_PDF_FILE = "../input/DATI_DI_TRAFFICO_2020.pdf"
 INPUT_ISTAT_2019_CSV_FILE = "../input/DCSC_INDTRAEREO_24052021180717528.csv"
 INPUT_AIRPORTS_CSV_FILE = "../input/anagrafica-aereoporti-nuovo-dataset.csv"
@@ -5,3 +7,11 @@ AIRPORTS_TRAFFIC_2020 = "../data/traffico_aereo_2020.csv"
 AIRPORTS_TRAFFIC_2019 = "../data/traffico_aereo_2019.csv"
 ITALIAN_AIRPORTS = "../data/aeroporti_italiani.csv"
 COMBINED_DATA = "../data/variazioni_traffico_aereo_2019_2020.csv"
+
+
+def files_are_present():
+    files = [AIRPORTS_TRAFFIC_2019, AIRPORTS_TRAFFIC_2020, ITALIAN_AIRPORTS, COMBINED_DATA]
+    if any([Path(file).is_file() for file in files]):
+        return True
+    else:
+        return False
